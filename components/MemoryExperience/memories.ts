8f6a9034feafb9d45a0e730e44b6d0ceb7d64010
Alexandre <alexandresmartinsbr@gmail.com>
2026-07-23 19:30:21 -0300

    add id na property memories

--- components/MemoryExperience/memories.ts
+++ components/MemoryExperience/memories.ts
@@ -1,44 +1,58 @@
 export const memories = [
     {
+        id: 1,
         image: "/photos/cartinha.jpg",
     },
     {
+        id: 2,
         image: "/photos/cartinha_chao.jpg",
     },
     {
+        id: 3,
         image: "/photos/beijo_evento.jpg",
     },
     {
+        id: 4,
         image: "/photos/declaracao.jpg",
     },
     {
+        id: 5,
         image: "/photos/declaracao_dois.jpg",
     },
     {
+        id: 6,
         image: "/photos/lego.jpg",
     },
     {
+        id: 7,
         image: "/photos/mensagem_cafeteria.jpg",
     },
     {
+        id: 8,
         image: "/photos/pedido.jpg",
     },
     {
+        id: 9,
         image: "/photos/petropolis_casa_alemao.jpg",
     },
     {
+        id: 10,
         image: "/photos/petropolis_onibus.jpg",
     },
     {
+        id: 11,
         image: "/photos/presente.jpg",
     },
     {
+        id: 12,
         image: "/photos/selfie_um.jpg",
     },
     {
+        id: 13,
         image: "/photos/stitch.jpg",
     },
     {
+        id: 14,
         image: "/photos/treino_dois.jpg",
     },
 ];
